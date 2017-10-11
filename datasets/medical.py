b214563cd05033ca44721438b7c640bd8301eb44
Sangwon Lee <leee.sangwon@gmail.com>
2017-10-11 16:47:25 +0900

    add medical code

---
+++ datasets/medical.py
@@ -0,0 +1,28 @@
+# Copyright 2017 Sangwon Lee. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Provides data for the Pascal VOC Dataset (images + annotations).
+"""
+import tensorflow as tf
+from datasets import pascalvoc_common
+
+slim = tf.contrib.slim
+
+FILE_PATTERN = 'medical_%s_*.record'
+ITEMS_TO_DESCRIPTIONS = {
+    'image': 'A color image of varying height and width.',
+    'shape': 'Shape of the image',
+    'object/bbox': 'A list of bounding boxes, one per each object.',
+    'object/label': 'A list of labels, one per each object.',
+}
