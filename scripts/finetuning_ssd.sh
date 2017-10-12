66561e64ed27f3447cfeb2ab93df0c25c4cb3ad1
Sangwon Lee <leee.sangwon@gmail.com>
2017-10-12 17:48:34 +0900

    add finetuing scripts for linux

---
+++ scripts/finetuning_ssd.sh
@@ -0,0 +1,17 @@
+DATASET_DIR=./DATA/medical
+TRAIN_DIR=./logs/ssd_300_vgg_medical
+CHECKPOINT_PATH=./checkpoints/ssd_300_vgg.ckpt
+python train_ssd_network.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=medical \
+    --dataset_split_name=train \
+    --model_name=ssd_300_vgg \
+    --checkpoint_path=${CHECKPOINT_PATH} \
+    --save_summaries_secs=60 \
+    --save_interval_secs=600 \
+    --weight_decay=0.0005 \
+    --optimizer=adam \
+    --learning_rate=0.001 \
+    --batch_size=32 \
+    --max_number_of_steps=1000
