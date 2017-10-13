f0ccc327eea9c56a69af354de537bfc369dcdd69
Sangwon Lee <leee.sangwon@gmail.com>
2017-10-13 14:19:53 +0900

    add scripts code

---
+++ scripts/evaluation_ssd.bat
@@ -0,0 +1,12 @@
+set DATASET_DIR=./DATA/PascalVOC/dataset
+set TRAIN_DIR=./logs/ssd_300_vgg_pascalVOC
+set EVAL_DIR=%TRAIN_DIR%/eval
+python eval_ssd_network.py ^
+    --eval_dir=%EVAL_DIR% ^
+    --dataset_dir=%DATASET_DIR% ^
+    --dataset_name=pascalvoc_2007 ^
+    --dataset_split_name=test ^
+    --model_name=ssd_300_vgg ^
+    --checkpoint_path=%TRAIN_DIR% ^
+    --wait_for_checkpoints=True ^
+    --batch_size=1
